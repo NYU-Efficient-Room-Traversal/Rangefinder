6a1aca6e8719b5a7b306e4036dc765516b5a2170
Cole Smith <css459@nyu.edu>
2017-04-30 17:16:17 -0400

    implemented rangefinding functions

--- range.go
+++ range.go
@@ -9,21 +9,32 @@
 
 package rangefinder
 
-import _ "fmt"
+import "math"
 
 // Returns the offset of the laser dot from the center
 // plane of the image in pixels. Value will be negative if
 // the laser dot is to the left of the center plane, and positive
 // if the dot is to the right of the center plane.
-func GetLaserOffset(image ImageMatrix) int {
-	var pixelOffset int
-	return pixelOffset
+func GetLaserOffset(image MonoImageMatrix) int {
+	var laserPlane int
+	centerPlane := image.width / 2
+	for w, _ := range image.image {
+		for _, v := range image.image[w] {
+			if v {
+				laserPlane = w
+			}
+		}
+	}
+
+	return laserPlane - centerPlane
 }
 
 // Returns the distance from the laser diode to the target based upon the
 // provided angle, at which the pixel offset was corrected by rotating the camera
 // such that the laser dot was in the center plane of the camera.
 func GetLaserDistance(angle float64, triangleBase float64) float64 {
-	var distance float64
-	return distance
+	sineLawBase := (triangleBase / math.Sin(angle))
+	sineOfAngleC := math.Sin(90 - angle)
+
+	return sineLawBase * sineOfAngleC
 }
